3c8d9fe15f126b4db3df24e1b9b7e7defd6d907c
Vishnu <165161462+vsnu4git@users.noreply.github.com>
2025-04-04 13:15:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,11 @@
-// Background Canvas
 const canvas = document.getElementById("background");
 const ctx = canvas.getContext("2d");
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-let particlesArray = [];
-const numParticles = 150;
+let particles = [];
 
-// Particle Constructor
 class Particle {
     constructor() {
         this.x = Math.random() * canvas.width;
@@ -16,21 +13,17 @@ class Particle {
         this.size = Math.random() * 5 + 1;
         this.speedX = Math.random() * 3 - 1.5;
         this.speedY = Math.random() * 3 - 1.5;
-        this.color = "cyan";
     }
 
     update() {
         this.x += this.speedX;
         this.y += this.speedY;
-
         if (this.x <= 0 || this.x >= canvas.width) this.speedX *= -1;
         if (this.y <= 0 || this.y >= canvas.height) this.speedY *= -1;
     }
 
     draw() {
-        ctx.fillStyle = this.color;
-        ctx.shadowBlur = 10;
-        ctx.shadowColor = "cyan";
+        ctx.fillStyle = "cyan";
         ctx.beginPath();
         ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
         ctx.closePath();
@@ -38,40 +31,27 @@ class Particle {
     }
 }
 
-// Initialize Particles
-function init() {
-    particlesArray = [];
-    for (let i = 0; i < numParticles; i++) {
-        particlesArray.push(new Particle());
+function initParticles() {
+    particles = [];
+    for (let i = 0; i < 100; i++) {
+        particles.push(new Particle());
     }
 }
-init();
 
-// Animate Particles
 function animate() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    particlesArray.forEach(p => {
+    particles.forEach(p => {
         p.update();
         p.draw();
     });
     requestAnimationFrame(animate);
 }
-animate();
 
-// Resize Canvas on Window Resize
 window.addEventListener("resize", () => {
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
-    init();
+    initParticles();
 });
 
-// 3D Floating Title with Three.js
-const title = document.getElementById("title");
-let titleScale = 1;
-
-function animateTitle() {
-    titleScale = Math.sin(Date.now() * 0.001) * 0.2 + 1;
-    title.style.transform = `scale(${titleScale})`;
-    requestAnimationFrame(animateTitle);
-}
-animateTitle();
+initParticles();
+animate();
